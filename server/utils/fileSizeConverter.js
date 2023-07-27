6ad2ead9463415c9c096d34cbb7fd14f0e18cc59
kobbin003 <dkfeto@gmail.com>
2023-07-27 12:27:45 +0530

    fileSizeConverter

---
+++ server/utils/fileSizeConverter.js
@@ -0,0 +1,18 @@
+export const fileSizeConverter = (fileSizeInBytes) => {
+	// use: toFixed()
+	// to: round numbers to a fixed number of decimals.
+	const fileSizeInKB = (fileSizeInBytes / 1024).toFixed(2);
+	const fileSizeInMB = (fileSizeInKB / 1024).toFixed(2);
+	const fileSizeInGB = (fileSizeInMB / 1024).toFixed(2);
+
+	// const fileUnit
+	const fileSize =
+		fileSizeInBytes < 100
+			? { fileSize: fileSizeInBytes, unit: "bytes" }
+			: fileSizeInKB < 1000
+			? { fileSize: fileSizeInKB, unit: "kb" }
+			: fileSizeInMB < 1000
+			? { fileSize: fileSizeInMB, unit: "MB" }
+			: { fileSize: fileSizeInGB, unit: "GB" };
+	return fileSize;
+};
