377a72f78299f39c981a754891f68e8efd21613e
kobbin003 <dkfeto@gmail.com>
2023-07-28 13:20:42 +0530

    /upload await

--- server/server.js
+++ server/server.js
@@ -5,9 +5,11 @@ import { extractRarArchive } from "./utils/extractRar.js";
 import path from "path";
 import session from "express-session";
 import cors from "cors";
+import { createExtractorFromData } from "node-unrar-js";
+import { readExtractedDir } from "./utils/readExtractedDir.js";
+
 const app = express();
 app.use(cors({ origin: "http://localhost:3001" }));
-const __dirname = path.dirname("./");
 console.log(path.join("hi", "bye"));
 app.use(
 	session({
@@ -23,78 +25,117 @@ app.use(
 		saveUninitialized: true,
 	})
 );
-
+// const __dirname = path.dirname(".");
+const __filename = new URL(import.meta.url).pathname;
+export const __dirname = path.dirname(__filename);
 app.get("/", (req, res) => {
-	console.log("id", req.session.id);
 	// Access and modify session data
 	req.session.views = req.session.views ? req.session.views + 1 : 1;
 
-	res.send(`Hello! You have visited this page ${req.session.views} times.`);
+	// res.send(
+	// 	`Hello! You(${req.session.id}) have visited this page ${req.session.views} times.`
+	// );
+	res.send({ id: req.session.id });
 });
-const uniqueDirPath = `./dir_${uuidv4()}`;
-let items = [];
-app.get("/readExtractedDir", async (req, res) => {
-	// empty items array if not empty
-	if (items.length > 0) {
-		items = [];
-	}
-	//extract the rar file & place it in 'uniqueDirPath'
-	await extractRarArchive("./sample.rar", uniqueDirPath);
-	// fs.readFile("./sample-1_1.webp", (err, data) => res.sendFile(data));
-	fs.readdir(uniqueDirPath, (err, files) => {
-		if (err) throw err;
-		/* //* with promise - CHATGPT */
-		files.map((item) => {
-			let isDir = false;
-			fs.stat(`${uniqueDirPath}/${item}`, (err, stats) => {
-				if (err) {
-					console.error(err);
-					return;
-				}
 
-				if (stats.isFile()) {
-					item = "file";
-					console.log("It is a file.");
-				} else if (stats.isDirectory()) {
-					isDir = true;
-					console.log("It is a directory.");
-				} else {
-					console.log("It is neither a file nor a directory.");
-				}
-			});
-			items.push({ id: uuidv4(), item, isDir });
-			// items.push(item);
-			// return items;
-		});
-		res.send(items);
+app.post("/upload", async (req, res) => {
+	const uploadSession = req.session.id;
+	const uploadDir = path.join(__dirname, uploadSession, "uploads");
+	try {
+		//* use promise to wait for directory to be created
+		//* OR
+		//* use mkdirSync
+		await fs.promises.mkdir(uploadDir, { recursive: true });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Failed to create directory" });
+	}
+	const filePath = path.join(uploadDir, "uploaded.rar");
+	//* create a writestream to create a file
+	const fileStream = fs.createWriteStream(filePath, {
+		encoding: "binary",
 	});
+	//* Pipe the request stream to the writestream
+	req.pipe(fileStream);
+	//* Wait for the file stream to finish writing
+	await new Promise((resolve, reject) => {
+		fileStream.on("finish", resolve);
+		fileStream.on("error", reject);
+	});
+
+	console.log("File write completed.");
+
+	//* extract the rar file & place it in 'uniqueDirPath'
+	const extractRarPath = path.join(__dirname, uploadSession, "extracts");
+	await extractRarArchive(filePath, extractRarPath);
+
+	//* read the extractedDir (ASYNC - AWAIT)
+	const extractedFiles = await readExtractedDir(extractRarPath);
+	//  send the upload session to the user
+	res.json({ sessionId: uploadSession, extractedFiles });
+
+	//* read the extractedDir (ASYNC -  then())
+	// readExtractedDir(extractRarPath).then((extractedFiles) =>
+	// 	//* send the upload session to the user
+	// 	res.json({ sessionId: uploadSession, extractedFiles })
+	// );
+
+	//* read the extractedDir ( CALLBACK : works for both async and non-async function )
+	// readExtractedDir(extractRarPath, (extractedFiles) =>
+	// 	res.json({ sessionId: uploadSession, extractedFiles })
+	// );
 });
-//* use resetSession route to clean up folder i.e uniqueDirPath.
-app.get("/resetSession", (req, res) => {
+app.post("/createFile", (req, res) => {
+	//* JUST A DEMO ROUTE TO PRACTICE CREATING FILE FROM STREAMS
+	console.log("/file");
+
+	//* WAY - 1 [creates file from "data" event of req's "on" method]
+	let chunks = [];
+	req.on("data", (data) => {
+		console.log("data", data);
+		chunks.push(data);
+	});
+	req.on("end", () => {
+		const payload = Buffer.concat(chunks);
+		console.log("payload", payload);
+		fs.writeFileSync("frombufferrar.rar", payload);
+	});
+
+	//* WAY - 2 [creates file from stream]
+	const fileStream = fs.createWriteStream("output.rar", { encoding: "binary" });
+	// Pipe the request stream to the file stream
+	req.pipe(fileStream);
+	// res.download(req.body);
+});
+
+const uniqueDirPath = `./dir_${uuidv4()}`;
+
+//* use resetSession route to clean up folder i.e session  Folder
+app.get("/resetSession/:id", (req, res) => {
+	const sessionId = req.params.id;
 	// Destroy the session to reset the visit count
 	req.session.destroy(() => {
-		fs.rmSync(uniqueDirPath, { recursive: true });
+		fs.rmSync(sessionId, { recursive: true });
 
-		res.send("Session has been reset.");
+		res.send("Session Dorectory has been deleted.");
 	});
 });
 //* API to download individual item
-app.get("/download/:id", async (req, res) => {
-	const id = req.params.id;
-	const queryPath = req.query.path;
+// app.get("/download/:id", async (req, res) => {
+// 	const id = req.params.id;
+// 	const queryPath = req.query.path;
 
-	const filteredFile = items && items.filter((item) => item.id == id);
-	const fileName = filteredFile[0].item;
-	const filePath = queryPath
-		? path.join(uniqueDirPath, queryPath, fileName)
-		: path.join(uniqueDirPath, fileName);
-	// const filePath = `${uniqueDirPath}/${fileName}`;
-	console.log("filePath", fileName, filePath);
-	res.download(filePath, fileName, (err) => {
-		if (err) {
-			res.send("error");
-			console.error(err);
-		}
-	});
-});
+// 	const filteredFile = items && items.filter((item) => item.id == id);
+// 	const fileName = filteredFile[0].item;
+// 	const filePath = queryPath
+// 		? path.join(uniqueDirPath, queryPath, fileName)
+// 		: path.join(uniqueDirPath, fileName);
+// 	console.log("filePath", fileName, filePath);
+// 	res.download(filePath, fileName, (err) => {
+// 		if (err) {
+// 			res.send("error");
+// 			console.error(err);
+// 		}
+// 	});
+// });
 app.listen(3000, () => console.log("server running at 3000"));
