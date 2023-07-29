40cd8a31e4d3c2b83a1da7da3df3ba046b79253e
kobbin003 <dkfeto@gmail.com>
2023-07-29 11:59:58 +0530

    uploader & fileViewer

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,26 @@
 import Image from "next/image";
+import Uploader from "../Components/uploader/page";
+import { useState } from "react";
+import { ReceivedFile } from "@/Components/uploader/types";
+import FileViewer from "@/Components/fileViewer";
 
 export default function Home() {
+	//* recievedfile from request.
+	const [receivedFiles, setReceivedFile] = useState<ReceivedFile>();
+	//* show/hide the <FileViewer/> component.
+	const [showFileViewer, setShowFileViewer] = useState(false);
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			MAIN
+			<Uploader
+				setReceivedFile={setReceivedFile}
+				setShowFileViewer={setShowFileViewer}
+			/>
+			{showFileViewer && receivedFiles && (
+				<FileViewer
+					file={receivedFiles}
+					key={receivedFiles.sessionId}
+				/>
+			)}
 		</main>
 	);
 }
