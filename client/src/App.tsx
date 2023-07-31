fa5010e3f69170b2a196d824c812621210e5b064
kobbin003 <dkfeto@gmail.com>
2023-07-31 16:09:49 +0530

    root layout set

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import ExtractLayout from "./pages/RootFolderContent";
 import { ReceivedFile } from "./Components/uploader/types";
 import Uploader from "./Components/uploader";
 import { Outlet } from "react-router-dom";
