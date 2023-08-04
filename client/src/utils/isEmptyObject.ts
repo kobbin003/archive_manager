a09f115f773027ef30c3f8a8a397519da65996b8
kobbin003 <dkfeto@gmail.com>
2023-08-04 18:11:15 +0530

    isEmptyObject utils

---
+++ client/src/utils/isEmptyObject.ts
@@ -0,0 +1,7 @@
+function isEmptyObject(obj: {}) {
+	return (
+		obj !== null && typeof obj === "object" && Object.keys(obj).length === 0
+	);
+}
+console.log(isEmptyObject({ x: "q" }));
+export default isEmptyObject;
