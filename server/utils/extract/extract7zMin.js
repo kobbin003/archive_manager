0c63cc9ccfb3c526c3c225d767ae0f5b6670c93d
kobbin003 <dkfeto@gmail.com>
2023-08-24 19:52:45 +0530

    extract7zMin to return promise

--- server/utils/extract/extract7zMin.js
+++ server/utils/extract/extract7zMin.js
@@ -1,65 +1,47 @@
-import _7z from "7zip-min";
+// import _7z from "7zip-min";
+// import fs from "fs";
+// import path from "path";
+// /**
+//  * 7zip-min :
+//  * 7z
+//  * zip
+//  * gzip
+//  * bzip2
+//  * tar.bz
+//  * tar
+//  */
+
+// export function extract7zMin(filePath, destinationPath) {
+// 	console.log("7min working");
+// 	/** check if the "destination" folder exists or not
+// 	 * IF not, create the "destination" folder
+// 	 */
+// 	if (!fs.existsSync(destinationPath)) {
+// 		fs.mkdirSync(destinationPath, { recursive: true });
+// 	}
+// 	_7z.unpack(filePath, destinationPath, (err) => {
+// 		console.log("error", err);
+// 	});
+// }
+
 import fs from "fs";
-import path from "path";
-/**
- * 7zip-min :
- * 7z
- * zip
- * gzip
- * bzip2
- * tar.bz
- * tar
- */
+import _7z from "7zip-min";
 
-export function extract7zMin(filePath, destinationPath) {
-	/** check if the "destination" folder exists or not
-	 * IF not, create the "destination" folder
-	 */
+export async function extract7zMin(filePath, destinationPath) {
+	// console.log("7min working");
+
 	if (!fs.existsSync(destinationPath)) {
 		fs.mkdirSync(destinationPath, { recursive: true });
 	}
-	_7z.unpack(filePath, destinationPath, (err) => {
-		console.log("error", err);
-	});
-}
 
-export function compress7zMin(filePath, destinationPath, fileName, fileType) {
-	/** check if the "destination" folder exists or not
-	 * IF not, create the "destination" folder
-	 */
-	if (!fs.existsSync(destinationPath)) {
-		fs.mkdirSync(destinationPath, { recursive: true });
-	}
-	let ext;
-
-	switch (fileType) {
-		case "7z":
-			ext = "7z";
-			break;
-		case "bzip2":
-			ext = "bz";
-			break;
-		case "tarbzip2":
-			ext = "tar.bz";
-			break;
-		case "gzip":
-			ext = "gz";
-			break;
-		case "tar":
-			ext = "tar";
-			break;
-
-		case "zip":
-			ext = "zip";
-			break;
-		default:
-			break;
-	}
-	// const name = path.join(`${destinationPath}.${ext}`);
-	// const name = path.join(destinationPath, `some.7z`);
-	const name = path.join(destinationPath, `${fileName}.${ext}`);
-
-	_7z.pack(filePath, name, (err) => {
-		console.log("error", err);
+	return new Promise((resolve, reject) => {
+		_7z.unpack(filePath, destinationPath, (err) => {
+			if (err) {
+				console.error("error", err);
+				reject(err);
+			} else {
+				resolve();
+			}
+		});
 	});
 }
