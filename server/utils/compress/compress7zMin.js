a7a75e1c3c5b75749d72de10903dd130ce1f0d4f
kobbin003 <dkfeto@gmail.com>
2023-08-24 19:50:00 +0530

    compress7zMin

---
+++ server/utils/compress/compress7zMin.js
@@ -0,0 +1,43 @@
+import _7z from "7zip-min";
+import fs from "fs";
+import path from "path";
+export function compress7zMin(filePath, destinationPath, fileName, fileType) {
+	/** check if the "destination" folder exists or not
+	 * IF not, create the "destination" folder
+	 */
+	if (!fs.existsSync(destinationPath)) {
+		fs.mkdirSync(destinationPath, { recursive: true });
+	}
+	let ext;
+
+	switch (fileType) {
+		case "7z":
+			ext = "7z";
+			break;
+		case "bzip2":
+			ext = "bz";
+			break;
+		case "tarbzip2":
+			ext = "tar.bz";
+			break;
+		case "gzip":
+			ext = "gz";
+			break;
+		case "tar":
+			ext = "tar";
+			break;
+
+		case "zip":
+			ext = "zip";
+			break;
+		default:
+			break;
+	}
+	// const name = path.join(`${destinationPath}.${ext}`);
+	// const name = path.join(destinationPath, `some.7z`);
+	const name = path.join(destinationPath, `${fileName}.${ext}`);
+
+	_7z.pack(filePath, name, (err) => {
+		console.log("error", err);
+	});
+}
