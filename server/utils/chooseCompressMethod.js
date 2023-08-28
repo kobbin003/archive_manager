809365dd2f6ffc4310686a1628b57773b5a56d95
kobbin003 <dkfeto@gmail.com>
2023-08-28 15:56:16 +0530

    chooseCompressMethod util

---
+++ server/utils/chooseCompressMethod.js
@@ -0,0 +1,43 @@
+import { extract7zMin } from "./extract/extract7zMin.js";
+import { extractRarArchive } from "./extract/extractRar.js";
+import { extractTarGzip } from "./extract/extractTarGzip.js";
+import { extractTarXZ } from "./extract/extractTarXZ.js";
+import extractXZ from "./extract/extractXZ.js";
+
+export const chooseCompressMethod = async (fileType, filePath, extractPath) => {
+	switch (fileType) {
+		case "rar":
+			await extractRarArchive(filePath, extractPath);
+			return;
+		case "zip":
+			await extract7zMin(filePath, extractPath);
+			return;
+		case "7z":
+			await extract7zMin(filePath, extractPath);
+			return;
+		case "tar":
+			await extract7zMin(filePath, extractPath);
+			return;
+		case "gz":
+			await extract7zMin(filePath, extractPath);
+			return;
+		case "bz":
+			await extract7zMin(filePath, extractPath);
+			return;
+		case "tar.bz2":
+			await extract7zMin(filePath, extractPath);
+			return;
+		case "tar.gz":
+			await extractTarGzip(filePath, extractPath);
+			return;
+		case "tar.xz":
+			await extractTarXZ(filePath, extractPath);
+			return;
+		case "xz":
+			await extractXZ(filePath, extractPath);
+			return;
+
+		default:
+			return;
+	}
+};
